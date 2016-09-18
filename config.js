1b75cd6cb59c6c945c7796018248adf24dc61c0b
TRUONGHM <huyminhtruong2003@gmail.com>
2016-09-18 23:44:36 +0700

    Use handlebars template instead of ejs file.

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 const express = require('express'), 
 	mongoose = require('mongoose'), 
+	exphbs = require('express-handlebars'), 
+	session = require('express-session'), 
+	logger = require('morgan'), 
+	methodOverride = require('method-override'), 
 	passport = require('passport'), 
 	flash = require('connect-flash'), 
 	bodyParser = require('body-parser'), 
@@ -10,17 +14,27 @@ const express = require('express'),
 
 module.exports = function(app, io){
 	mongoose.connect('mongodb://localhost/socketio')
+
 	app.use(cookieParser())
 	app.use(bodyParser.json())
 	app.use(bodyParser.urlencoded({extended: false}))
 
-	app.set('view engine', 'ejs')
-	app.engine('ejs', require('ejs').renderFile)
-	app.set('views', __dirname + '/views')
-	
+	app.use(methodOverride('X-HTTP-Method-Override'))
+	app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}))
+
 	app.use(passport.initialize())
 	app.use(passport.session())
 	app.use(flash())
+
+	app.engine('.hbs', exphbs({
+		defaultLayout: 'main', 
+		extname: '.hbs', 
+		layoutsDir: path.join(__dirname, 'views/layouts'), 
+		partialsDir: path.join(__dirname, 'views/partials')
+	}))
+
+	app.set('view engine', '.hbs')
+	app.set('views', __dirname + '/views')
 
 	app.use(express.static(path.join(__dirname, '/public')))
 }
--- controllers/users.js
+++ controllers/users.js
@@ -5,16 +5,21 @@ module.exports = function(app, io){
 		multer = require('multer'), 
 		uploads = multer({dest: './public/uploads'}).single('file');
 
-	app.get('/', function(req,res){
+	app.get('/', (req,res) => {
 		Image.find({}).exec(function(error, images){
-			res.render('./home', {
-				images: images
+			res.render('./login', {
+				images: images,
+				method: '/signin',
+				title: 'Please sign in',
+				email: 'Email address',
+				password: 'Passwprd',
+				action: 'Sign in'
 			});
 		});
 	});
 
 	app.route('/users')
-	.get(function(req,res){
+	.get((req,res) => {
 		User.find({}).exec(function(error, users){
 			if(error){
 				console.log('Get error: ' + error);
@@ -23,8 +28,8 @@ module.exports = function(app, io){
 				users: users
 			});
 		});
-	}).post(uploads, function(req,res){
-		var name = req.body.name, 
+	}).post(uploads, (req,res) => {
+		const name = req.body.name, 
 			email = req.body.email,
 			password = req.body.password, 
 			file = 'data:image/png;base64,' 
@@ -41,8 +46,7 @@ module.exports = function(app, io){
 		});
 	});
 
-	app.get('/users/:id', function(req,res){
-		var mongoose = require('mongoose');
+	app.get('/users/:id', (req,res) => {
 		User.findById({_id: req.params.id}).exec(function(error,user){
 			res.render('./welcome', {
 				user: user
@@ -50,9 +54,10 @@ module.exports = function(app, io){
 		});
 	});
 
-	app.post('/signin', function(req,res){
-		var email = req.body.email;
-		var password = req.body.password;
+	app.post('/signin', uploads, (req,res) => {
+		const email = req.body.email
+		const password = req.body.password
+
 		User.findOne({email: email}).exec(function(error, user){
 			if (error) {
 				console.log(error);
@@ -65,23 +70,25 @@ module.exports = function(app, io){
 		});
 	});
 
-	app.get('/new', function(req,res){
+	app.get('/new', (req,res) => {
 		res.render('./register', {
-			title: '',
-			email: '',
-			avatar: ''
+			method: '/users',
+			title: 'Register New Account',
+			email: 'Your email',
+			password: 'Your password',
+			action: 'Register'
 		});
 	});
 
 	app.route('/images')
-	.get(function(req,res){
+	.get((req,res) => {
 		Image.find({}, function(error, images){
 			res.render('./images', {
 				images: images
 			});
 		});
-	}).post(uploads, function(req,res,next){
-		var image = new Image();
+	}).post(uploads, (req,res,next) => {
+		const image = new Image();
 		image.name = req.body.name;
 		image.image = 'data:image/png;base64,' 
 		+ fs.readFileSync(req.file.path).toString('base64');
@@ -94,9 +101,9 @@ module.exports = function(app, io){
 	});
 
 	app.route('/emails')
-	.get(function(req,res){
+	.get((req,res) => {
 		res.render('./email', {});
-	}).post(function(req,res){
+	}).post((req,res) => {
 		
 	});
 }
